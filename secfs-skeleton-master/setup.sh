f9928f70f4b630b696364fca61b771a1e9750d0d
erenphy <lixiaohui_ouc@163.com>
2024-08-19 17:23:50 +0800

    add pip ungrade

--- secfs-skeleton-master/setup.sh
+++ secfs-skeleton-master/setup.sh
@@ -4,6 +4,7 @@ set -e
 rm -rf venv
 python3 -m venv --clear venv
 . venv/bin/activate
+python3 -m pip install --upgrade pip
 python3 -m pip install wheel
 python3 -m pip install --upgrade -e .
 deactivate
